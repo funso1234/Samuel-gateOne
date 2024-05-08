6591c408f10ed7658cb022fe7ab483748480f544
funso1234 <samuel08063251499@gmail.com>
2024-05-08 13:26:04 +0100

    commit

---
+++ GateOne/Day three/java/TaskOne.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class TaskOne {
+
+	public static void main(String...args) {
+
+		Scanner input = new Scanner(System.in);
+		Random random = new Random();
+		
+		for (int counter = 0; counter < 10; counter++) {
+
+		System.out.println("Random integers"+ rand_int);
+		int integer = random.nextInt();
+		
+		System.out.println(setUp + "x" + newOne + "=");
+
+		int sum = random.nextInt();
+
+		if (sum == setUp * newOne){
+			correct.add(num);		
+		}
+
+		else{
+			incorrect.add(num);
+
+		}
+
+		}
+
+	}
+
+
+
+
+}
---
+++ GateOne/Day three/javascript/TaskOne.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+let count = 0; count < 5; count++
+
+	const randomNumber1 = Math.floor(math.rndom() * 10) + 1;
+
+console.log(randomNumber);
