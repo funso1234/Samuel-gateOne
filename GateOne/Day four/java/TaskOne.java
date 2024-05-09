d0dc825e644e61ab3d01f455a14ff8d4f172cdb3
funso1234 <samuel08063251499@gmail.com>
2024-05-09 14:01:19 +0100

    commit

---
+++ GateOne/Day four/java/TaskOne.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+public class TaskOne {
+public static void main(String...args) {
+
+	Scanner scanner = new Scanner(System.in);
+
+	char[][] array = new char[3][3];
+
+	for (int counter = 0; counter < 3; counter++){
+
+	for (int count = 0; count < 3; count++){
+
+	System.out.println("Enter X or O");
+
+	String userInput = scanner.nextLine();
+
+	System.out.println("Choose from 1 to 9");
+
+	int input2 = scanner.nextInt();
+	
+	
+
+		if (input == 1){
+
+		array[0] = userInput;
+	}
+
+	else if (input2 == 2){
+
+		array[0] = userInput;
+	
+	}	
+	
+	else if (input2 == 3){
+
+		array[0] = userInput;
+	
+	}
+
+	else if (input2 == 4){
+
+		array[0] = userInput;
+	
+	}	
+	
+	else if (input2 == 5){
+
+		array[0] = userInput;
+	
+	}	
+
+	else if (input2 == 6){
+
+		array[0] = userInput;
+	
+	}
+
+	else if (input2 == 7){
+
+		array[0] = userInput;
+	
+	}
+
+	else if (input2 == 8){
+
+		array[0] = userInput;
+	
+	}	
+	
+	else if (input2 == 9){
+
+		array[0] = userInput;
+	
+	}	
+
+	}
+	}
+
+
+		
+}
+
+}
---
+++ GateOne/Day four/python/TaskOne.py
@@ -0,0 +1,9 @@
+for input in range (9):
+
+	user_input1 = str(input("Enter X or O"))
+
+	user_input2 = int(input("Choose from 1 to 9"))
+
+	user_output = user_input
+
+print(user_output)
