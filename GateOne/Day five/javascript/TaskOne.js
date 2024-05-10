8e3016099b2a6ad707110722934a700bd0e0a4fb
funso1234 <samuel08063251499@gmail.com>
2024-05-10 12:58:17 +0100

    commit

---
+++ GateOne/Day five/javascript/TaskOne.js
@@ -0,0 +1,41 @@
+const prompt = require("prompt-sync")();
+
+let validateNumber;
+
+	if (validateNumber.charAt(0)=='4'||validateNumber.charAt(0)=='5'|| validateNumber.charAt(0)=='6') {
+
+console.log(validateNumber.charAt(0));
+		
+console.log(validateNumber.charAt(1));
+
+console.log(validateNumber.charAt(2));
+
+console.log(validateNumber.charAt(3) + "-");
+
+console.log(validateNumber.charAt(4));
+
+console.log(validateNumber.charAt(5));
+
+console.log(validateNumber.charAt(6));
+		
+console.log(validateNumber.charAt(7) + "-");
+
+console.log(validateNumber.charAt(8));
+
+console.log(validateNumber.charAt(9));
+
+console.log(validateNumber.charAt(10));
+
+console.log(validateNumber.charAt(11) + "-");
+
+console.log(validateNumber.charAt(12));
+
+console.log(validateNumber.charAt(13));
+
+console.log(validateNumber.charAt(14));
+
+console.log(validateNumber.charAt(15));
+
+
+}
+
