a43b356141b91c9c4024dab9d3850edea644dee6
funso1234 <samuel08063251499@gmail.com>
2024-05-13 15:01:52 +0100

    commit

---
+++ GateOne/Day eight/phoneBook/PhoneBook.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class PhoneBook {
+
+public static void main(String...args){
+
+	Scanner input = new Scanner(System.in);
+
+	ArrayList<Contact> list = new ArrayList<Contact>();
+
+	System.out.println("Please enter Add contact");
+
+	String addContact = input.nextLine();
+
+	System.out.println("Please enter Remove contact");
+
+	String removeContact = input.nextLine();
+
+
+}
+
+}
