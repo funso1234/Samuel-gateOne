717224ce19a7b5b21458ea52324fd49e6a09669f
funso1234 <samuel08063251499@gmail.com>
2024-05-14 04:53:05 +0100

    commit

--- GateOne/Day seven/MenstrualWahala/MenstrualCycle.java
+++ GateOne/Day seven/MenstrualWahala/MenstrualCycle.java
@@ -1,20 +1,29 @@
-import java.util.Date;
+import java.time.LocalDate;
+
 import java.util.Scanner;
+
 public class MenstrualCycle {
+
 public static void main(String...args) {
+
 	Scanner input = new Scanner(System.in);
+
+	LocalDate now = LocalDate.now();
 
 
 	System.out.print("Enter your last menstrual flow date (yyyy-mm-dd): ");
-        LocalDate lastDate = LocalDate.parse(scanner.nextLine());
+
+        LocalDate lastDate = LocalDate.parse(input.nextLine());
 
         System.out.print("Enter the average menstrual cycle length in days: ");
-        int cycleLength = scanner.nextInt();
+
+        int cycleLength = input.nextInt();
 
         LocalDate nextMenstrualFlow = lastDate.plusDays(cycleLength);
 
-        LocalDate ovulationDate = lastDate.plusDays(cycleLength / 2);
+        LocalDate ovulationDate = lastDate.plusDays(cycleLength);
 
+	
 }
 
 }
