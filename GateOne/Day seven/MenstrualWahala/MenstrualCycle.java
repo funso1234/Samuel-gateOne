96a2ebdad97b71282b274d04abb5605809427f06
funso1234 <samuel08063251499@gmail.com>
2024-05-16 07:49:30 +0100

    commit

--- GateOne/Day seven/MenstrualWahala/MenstrualCycle.java
+++ GateOne/Day seven/MenstrualWahala/MenstrualCycle.java
@@ -2,6 +2,8 @@ import java.time.LocalDate;
 
 import java.time.format.DateTimeFormatter;
 
+import java.time.temporal.ChronoUnit;
+    
 import java.util.Scanner;
 
 public class MenstrualCycle {
@@ -10,33 +12,39 @@ public static void main(String...args) {
 
 	Scanner input = new Scanner(System.in);
 
-	LocalDate now = LocalDate.now();
+	LocalDate today = LocalDate.now();
 
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
-	System.out.println("Enter the first day of menstrual flow (yyyy-mm-dd): ");
+	System.out.println("Enter the start date of menstrual flow (dd/MM/yyyy): ");
 	String firstDay = input.nextLine();
 
-	System.out.println("Enter your last day of menstrual flow (yyyy-mm-dd): ");
+	System.out.println("Enter the last date of your previous menstrual flow (dd/MM/yyyy): ");
 	String lastDay = input.nextLine();
-
-	LocalDate lastDateOfFlow = LocalDate.parse(lastDay, DateTimeFormatter.ofPattern("yyyy-mm-dd"));
-
-	System.out.println("Enter the day of your menstrual flow: "); 
-	int daysOfMenstrualFlow = input.nextInt();
+         
+        LocalDate start = LocalDate.parse(firstDay,formatter);
+	LocalDate end = LocalDate.parse(lastDay,formatter);
 
         System.out.print("Enter the average menstrual cycle length in days: ");
 	int cycleLength = input.nextInt();
 
+        int lengthToOvulation = cycleLength / 2;
 
-	LocalDate today = LocalDate.now();
+        LocalDate nextMenstrualFlow = start.plusDays(cycleLength);
 
-        LocalDate lastDate = LocalDate.parse(input.nextLine());
+        LocalDate ovulationDate = start.plusDays(lengthToOvulation);
 
-        LocalDate nextMenstrualFlow = lastDate.plusDays(cycleLength);
+        LocalDate lengthFertilityTime = start.plusDays(9);
 
-        LocalDate ovulationDate = lastDate.plusDays(cycleLength);
+	LocalDate safetimer = end.minusDays(4);
 
+        LocalDate safetime = ovulationDate.plusDays(5);
 
+        System.out.println("Your Expected Ovulation Date is "  + ovulationDate);
+
+        System.out.println("Your Expected Fertility Period is " + lengthFertilityTime + " to " + ovulationDate);
+
+        System.out.println("Your safe period is " + safetime + " to " + safetimer);
 	
 }
 
